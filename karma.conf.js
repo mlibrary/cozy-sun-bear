407575e7921546067dd6b4110d5af9b12cf182f3
Roger Espinosa <roger@umich.edu>
2017-10-16 14:53:18 -0400

    modal specs

--- karma.conf.js
+++ karma.conf.js
@@ -104,6 +104,12 @@ module.exports = function (config) {
 			// }
 		},
 
+		browserConsoleLogOptions: {
+		      level: 'log',
+		      format: '%b %T: %m',
+		      terminal: true
+		},
+
 		// If browser does not capture in given timeout [ms], kill it
 		captureTimeout: 5000,
 
