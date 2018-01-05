919766e4c66efa96eadc938498b00bb8283219d4
Roger Espinosa <roger@umich.edu>
2018-01-05 16:19:19 -0500

    have reader load the necessary engine

--- src/reader/index.js
+++ src/reader/index.js
@@ -7,51 +7,7 @@ var engines = {
   mock: Mock.createReader
 }
 
-var load = (function() {
-  // Function which returns a function: https://davidwalsh.name/javascript-functions
-  function _load(tag) {
-    return function(url) {
-      // This promise will be used by Promise.all to determine success or failure
-      return new Promise(function(resolve, reject) {
-        var element = document.createElement(tag);
-        var parent = 'body';
-        var attr = 'src';
-
-        // Important success and error for the promise
-        element.onload = function() {
-          resolve(url);
-        };
-        element.onerror = function() {
-          reject(url);
-        };
-
-        // Need to set different attributes depending on tag type
-        switch(tag) {
-          case 'script':
-            element.async = true;
-            break;
-          case 'link':
-            element.type = 'text/css';
-            element.rel = 'stylesheet';
-            attr = 'href';
-            parent = 'head';
-        }
-
-        // Inject into document to kick off loading
-        element[attr] = url;
-        document[parent].appendChild(element);
-      });
-    };
-  }
-  
-  return {
-    css: _load('link'),
-    js: _load('script'),
-    img: _load('img')
-  }
-})();
-
-var xload = {
+var load = {
 	js: function(url) {
 		var handler = {};
 		handler.callbacks = [];
@@ -91,7 +47,6 @@ var xload = {
 		  handler.resolve(url);
 		};
 		element.onerror = function() {
-		  // console.log("BAD THINGS HAPPANED", url);
 		  handler.catch.apply(arguments);
 		};
 
@@ -116,10 +71,7 @@ export var reader = function(id, options) {
   	return engines[engine].apply(_this, _arguments);
   }
 
-  // return engines[engine].apply(this, arguments);
-  console.log("AHOY LOADING", engine_href, options.engine, engine);
-  return xload.js(engine_href).then(function() {
-  	console.log("AHOY LOADED epub.js", engine_href, engine);
+  return load.js(engine_href).then(function() {
     return engines[engine].apply(_this, _arguments);
   }).catch(function(e) {
     console.log('Oh no, epic failure!', e);
