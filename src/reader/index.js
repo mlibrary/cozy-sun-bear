f50db244600614c445008525757e543e41e452a4
Roger Espinosa <roger@umich.edu>
2018-01-09 14:58:32 -0500

    not using this

--- src/reader/index.js
+++ src/reader/index.js
@@ -7,60 +7,6 @@ var engines = {
   mock: Mock.createReader
 }
 
-var load = {
-	js: function(url) {
-		var handler = {};
-		handler.callbacks = [];
-		handler.error = [];
-		handler.then = function(cb) {
-			handler.callbacks.push(cb);
-			return handler;
-		}
-		handler.catch = function(cb) {
-			handler.error.push(cb);
-			return handler;
-		}
-		handler.resolve = function(_argv) {
-			// var _argv;
-			while ( handler.callbacks.length ) {
-				var cb = handler.callbacks.shift();
-				var retval;
-				try {
-					_argv = cb(_argv);
-				} catch(e) {
-					handler.reject(e);
-					break;
-				}
-			}
-		}
-
-		handler.reject = function(e) {
-			while ( handler.error.length ) {
-				var cb = handler.error.shift();
-				cb(e);
-			}
-		}
-
-		var element = document.createElement('script');
-
-		element.onload = function() {
-		  handler.resolve(url);
-		};
-		element.onerror = function() {
-		  handler.catch.apply(arguments);
-		};
-
-		element.async = true;
-		var parent = 'body';
-		var attr = 'src';
-		element[attr] = url;
-		document[parent].appendChild(element);
-
-		return handler;
-	}
-}
-
-
 export var reader = function(id, options) {
   options = options || {};
   var engine = options.engine || window.COZY_EPUB_ENGINE || 'epubjs';
