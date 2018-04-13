752bc77328e117d809f4118dde4c86fea50a2111
Roger Espinosa <roger@umich.edu>
2018-04-13 14:55:54 -0400

    text size ux

--- src/control/Control.Preferences.js
+++ src/control/Control.Preferences.js
@@ -50,13 +50,30 @@ export var Preferences = Control.extend({
 
   _createPanel: function() {
     var self = this;
-    var template = `<fieldset>
+    var template = '';
+
+    if ( self._reader.metadata.layout == 'pre-paginated' ) {
+      // something else
+    } else {
+
+      template += `<fieldset class="cozy-fieldset-text_size">
         <legend>Text Size</legend>
-        <label><input name="text_size" type="radio" id="preferences-input-size-small" value="small" />Small</label>
-        <label><input name="text_size" type="radio" id="preferences-input-size-auto" value="auto" />Default</label>
-        <label><input name="text_size" type="radio" id="preferences-input-size-large" value="large" />Large</label>
-      </fieldset>          
-      <fieldset>
+        <div class="preview--text_size">
+          ‘Yes, that’s it,’ said the Hatter with a sigh: ‘it’s always tea-time, and we’ve no time to wash the things between whiles.’
+        </div>
+        <p style="white-space: no-wrap">
+          <span>T-</span>
+          <input name="text_size" type="range" id="preferences-input-text_size" value="100" min="50" max="400" step="10" style="width: 75%" />
+          <span>T+</span>
+        </p>
+        <p>
+          <span>Text Size: </span>
+          <output for="preferences-input-text_size" id="output-text_size">100</output>
+        </p>
+      </fieldset>`;
+    }
+
+    template += `<fieldset>
         <legend>Text Display</legend>
         <label><input name="flow" type="radio" id="preferences-input-paginated" value="paginated" />Page-by-Page</label>
         <label><input name="flow" type="radio" id="preferences-input-scrolled-doc" value="scrolled-doc" />Scroll</label>
@@ -118,16 +135,30 @@ export var Preferences = Control.extend({
   _initializeForm: function() {
     var input, input_id;
     this._lastValues = {};
-    /// input_id = "preferences-input-" + ( this._reader.options.flow == 'scrolled-doc' ? 'scrollable' : 'reflowable' );
-    input_id = "preferences-input-" + ( this._reader.options.flow == 'auto' ? 'paginated' : 'scrolled-doc' );
+    var flow = this._reader.options.flow;
+    if ( flow == 'auto' ) { flow = 'paginated'; }
+    input_id = "preferences-input-" + flow;
     input = this._form.querySelector("#" + input_id);
     input.checked = true;
     input.parentElement.parentElement.dataset.last = input.value;
 
-    input_id = "preferences-input-size-" + ( this._reader.options.text_size || 'auto' );
-    input = this._form.querySelector("#" + input_id);
-    input.checked = true;
+    input_id = "preferences-input-text_size";
+    input = this._form.querySelector('#' + input_id);
+    var text_size = this._reader.options.text_size || 100;
+    if ( text_size == 'auto' ) { text_size = 100; }
+    input.value = text_size;
     input.parentElement.parentElement.dataset.last = input.value;
+
+    var text_size_preview = this._form.querySelector('.preview--text_size');
+    var text_size_output = this._form.querySelector('#output-text_size');
+    var update_text_size_preview = function() {
+      text_size_preview.style.fontSize = `${( parseInt(this.value, 10) / 100 )}em`;
+      text_size_output.value = `${this.value}%`;
+    }
+
+    input.addEventListener('input', update_text_size_preview);
+    input.addEventListener('change', update_text_size_preview);
+    update_text_size_preview.bind(input).call();
 
     if ( this.options.hasThemes ) {
       input_id = "preferences-input-theme-" + ( this._reader.options.theme || 'default' );
@@ -148,7 +179,7 @@ export var Preferences = Control.extend({
     options.flow = input.value;
     input.parentElement.parentElement.dataset.last = input.value;
 
-    input = this._form.querySelector("input[name='text_size']:checked");
+    input = this._form.querySelector("input[name='text_size']");
     doUpdate = doUpdate || ( input.value != input.parentElement.parentElement.dataset.last );
     options.text_size = input.value;
     input.parentElement.parentElement.dataset.last = input.value;
@@ -176,6 +207,7 @@ export var Preferences = Control.extend({
 
     if ( doUpdate ) {
       setTimeout(function() {
+        self._reader.saveOptions(options);
         self._reader.reopen(options);
       }, 100);
     }
