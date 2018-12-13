b9d8d5aa0b8bb72fad781e59f7c66cbec380cc95
Roger Espinosa <roger@umich.edu>
2018-12-13 15:28:28 -0500

    add spec for new approach

--- karma.conf.js
+++ karma.conf.js
@@ -16,137 +16,137 @@ module.exports = function (config) {
   var files = [
     "node_modules/sinon/pkg/sinon.js",
     "node_modules/expect.js/index.js",
-		"node_modules/happen/happen.js",
-		"node_modules/prosthetic-hand/dist/prosthetic-hand.js",
-		"spec/SpecHelper.js",
-		{pattern: 'spec/fixtures/**/*', watched: false, included: false, served: true},
-		{pattern: 'vendor/**/*', watched: false, included: false, served: true}
-		// "spec/**/*Spec.js"
+    "node_modules/happen/happen.js",
+    "node_modules/prosthetic-hand/dist/prosthetic-hand.js",
+    "spec/SpecHelper.js",
+    {pattern: 'spec/fixtures/**/*', watched: false, included: false, served: true},
+    {pattern: 'vendor/**/*', watched: false, included: false, served: true}
+    // "spec/**/*Spec.js"
   ];
 
-  if ( process.env.KARMA_USE_DIST) {
-  	files.unshift("dist/cozy-sun-bear.js");
+  if ( process.env.KARMA_USE_DIST ) {
+    files.unshift("dist/cozy-sun-bear.js");
   } else {
-  	files.unshift("src/cozy.js");
+    files.unshift("src/cozy.js");
   }
 
-	var exclude = [
+  var exclude = [
   ];
 
-	config.set({
+  config.set({
     // base path, that will be used to resolve files and exclude
-		basePath: '',
+    basePath: '',
 
-		// plugins
-		plugins: [
-			'karma-rollup-preprocessor',
-			'karma-mocha',
-			'karma-coverage',
-			'karma-coveralls',
-			'karma-sourcemap-loader',
-			// 'karma-phantomjs-launcher',
-			// 'karma-jsdom-launcher',
-			'karma-chrome-launcher',
-			'karma-safari-launcher',
-			'karma-firefox-launcher'],
+    // plugins
+    plugins: [
+      'karma-rollup-preprocessor',
+      'karma-mocha',
+      'karma-coverage',
+      'karma-coveralls',
+      'karma-sourcemap-loader',
+      // 'karma-phantomjs-launcher',
+      // 'karma-jsdom-launcher',
+      'karma-chrome-launcher',
+      'karma-safari-launcher',
+      'karma-firefox-launcher'],
 
-		// frameworks to use
-		frameworks: ['mocha'],
+    // frameworks to use
+    frameworks: ['mocha'],
 
-		// list of files / patterns to load in the browser
-		files: files.concat(getSpecs(process.env.KARMA_SPECS)),
+    // list of files / patterns to load in the browser
+    files: files.concat(getSpecs(process.env.KARMA_SPECS)),
 
-		// list of files / patterns to exclude from invoking
-		exclude: exclude,
+    // list of files / patterns to exclude from invoking
+    exclude: exclude,
 
-		// Rollup the ES6 Cozy sources into just one ES5 file, before tests
-		preprocessors: {
-			// 'src/cozy.js': ['rollup', 'coverage']
-			'src/*.js': [ 'rollup', 'sourcemap' ]
-		},
-		rollupPreprocessor: {
-			plugins: [
-				json(),
-		    nodeResolve({ jsnext: true, main: true, browser: true, preferBuiltins: true }),
-				commonjs(),
-				buble()
-			],
-			output: {
-				format: 'umd',
-				name: 'cozy',
-				sourcemap: 'inline'
-			},
-			// moduleName: 'cozy' //,
-			// sourceMap: 'inline'
-		},
+    // Rollup the ES6 Cozy sources into just one ES5 file, before tests
+    preprocessors: {
+      // 'src/cozy.js': ['rollup', 'coverage']
+      'src/*.js': [ 'rollup', 'sourcemap' ]
+    },
+    rollupPreprocessor: {
+      plugins: [
+        json(),
+        nodeResolve({ jsnext: true, main: true, browser: true, preferBuiltins: true }),
+        commonjs(),
+        buble()
+      ],
+      output: {
+        format: 'umd',
+        name: 'cozy',
+        sourcemap: 'inline'
+      },
+      // moduleName: 'cozy' //,
+      // sourceMap: 'inline'
+    },
 
-		// test results reporter(s) to use possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-		reporters: ['dots', 'coverage', 'coveralls', 'progress'],
-		coverageReporter: {
-			type: 'lcov',
-			dir: 'coverage/'
-		},
+    // test results reporter(s) to use possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['dots', 'coverage', 'coveralls', 'progress'],
+    coverageReporter: {
+      type: 'lcov',
+      dir: 'coverage/'
+    },
 
-		// web server port
-		port: 9876,
+    // web server port
+    port: 9876,
 
-		// level of logging
-		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-		// logLevel: config.LOG_INFO,
-		logLevel: config.LOG_DEBUG,
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    // logLevel: config.LOG_INFO,
+    logLevel: process.env.LOG_LEVEL ? config[process.env.LOG_LEVEL] : config.LOG_INFO,
 
-		// enable / disable colors in the output (reporters and logs)
-		colors: true,
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
-		// enable / disable watching file and executing tests whenever any file changes
-		autoWatch: false,
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
 
-		// Start these browsers, currently available:
-		// - Chrome
-		// - ChromeCanary
-		// - Firefox
-		// - Opera
-		// - Safari (only Mac)
-		// - PhantomJS
-		// - IE (only Windows)
-		// browsers: ['PhantomJSCustom'],
-		browsers: ['ChromeHeadless'],
-		customLaunchers: {
-			// 'PhantomJSCustom': {
-			// 	base: 'PhantomJS',
-			// 	flags: ['--load-images=true'],
-			// 	options: {
-			// 		onCallback: function (data) {
-			// 			if (data.render) {
-			// 				page.render(data.render);
-			// 			}
-			// 		}
-			// 	}
-			// }
-		},
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    // browsers: ['PhantomJSCustom'],
+    browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      // 'PhantomJSCustom': {
+      //  base: 'PhantomJS',
+      //  flags: ['--load-images=true'],
+      //  options: {
+      //    onCallback: function (data) {
+      //      if (data.render) {
+      //        page.render(data.render);
+      //      }
+      //    }
+      //  }
+      // }
+    },
 
-		browserConsoleLogOptions: {
-		      level: 'log',
-		      format: '%b %T: %m',
-		      terminal: true
-		},
+    browserConsoleLogOptions: {
+          level: 'log',
+          format: '%b %T: %m',
+          terminal: process.env.USE_CONSOLE ? true : false
+    },
 
-		client: {
-			captureConsole: true,
-			mocha: {
-				bail: true
-			}
-		},
+    client: {
+      captureConsole: process.env.USE_CONSOLE ? true : false,
+      mocha: {
+        bail: true
+      }
+    },
 
-		// If browser does not capture in given timeout [ms], kill it
-		captureTimeout: 100000,
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 100000,
 
-		// Workaround for PhantomJS random DISCONNECTED error
-		browserDisconnectTimeout: 100000, // default 2000
-		browserDisconnectTolerance: 1, // default 0
+    // Workaround for PhantomJS random DISCONNECTED error
+    browserDisconnectTimeout: 100000, // default 2000
+    browserDisconnectTolerance: 1, // default 0
 
-		// Continuous Integration mode
-		// if true, it capture browsers, run tests and exit
-		singleRun: true
-	});
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: true
+  });
 };
