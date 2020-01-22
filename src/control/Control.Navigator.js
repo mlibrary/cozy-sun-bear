69fd17e0543fcc9d84b6e50bd7ac337031695528
Roger Espinosa <roger@umich.edu>
2020-01-22 13:26:46 -0500

    some bits handling significant navigation

--- src/control/Control.Navigator.js
+++ src/control/Control.Navigator.js
@@ -17,13 +17,6 @@ export var Navigator = Control.extend({
     this._setup(container);
 
     this._reader.on('updateLocations', function(locations) {      
-      // if ( ! this._reader.currentLocation() || ! this._reader.currentLocation().start ) {
-      //   console.log("AHOY updateLocations NO START", this._reader.currentLocation().then);
-      //   setTimeout(function() {
-      //     this._initializeNavigator(locations);
-      //   }.bind(this), 100);
-      //   return;
-      // }
       this._initializeNavigator(locations);
     }.bind(this));
 
@@ -68,8 +61,13 @@ export var Navigator = Control.extend({
     this._control.addEventListener("input", function() {
       self._update();
     }, false);
+<<<<<<< HEAD
     this._control.addEventListener("change", function(e) { self._action(); }, false);
     this._control.addEventListener("mousedown", function(){
+=======
+    this._control.addEventListener("change", function(event) { console.log("AHOY NAVIGATOR change", event); self._action(); }, false);
+    this._control.addEventListener("mousedown", function(event){
+>>>>>>> 8340f58... some bits handling significant navigation
         self._mouseDown = true;
     }, false);
     this._control.addEventListener("mouseup", function(){
@@ -85,16 +83,29 @@ export var Navigator = Control.extend({
     }, false);
 
     this._reader.on('relocated', function(location) {
+      var value; var percentage;
       if ( ! self._initiated ) { return ; }
-      if ( ! ( location && location.start && location.start.cfi ) ) { return ; }
-      var value = Math.ceil(self._reader.locations.percentageFromCfi(location.start.cfi) * 10000) / 100.0;
-      console.log("AHOY NAVIGATOR relocated", location, value, self._control.value);
-      if ( true || value != self._control.value ) {
-        self._control.value = value;
-        self._update(location);
-      }
-    })
+      if ( self._ignore ) { self._ignore = false; console.log("AHOY IGNORING", self._ignore); return; }
+      if ( ! ( location && location.start ) ) { return ; }
 
+      var value;
+      if ( location.start && location.end ) {
+        // EPUB
+        value = parseInt(self._control.value, 10);
+        var start = parseInt(location.start.location, 10);
+        var end = parseInt(location.end.location, 10);
+        console.log("AHOY NAVIGATOR relocated", value, start, end, value < start, value > end);
+        if ( value < start || value > end ) {
+          self._last_value = value;
+          self._control.value = ( value < start ) ? start : end;
+        }
+      } else {
+        value = self._parseLocation(location);
+        self._last_value = value;
+        self._control.value = value;
+      }
+
+<<<<<<< HEAD
     this._reader.on('xxrelocated', function(location) {
       console.log("AHOY NAVIGATOR relocated", location, self._initiated, self._mouseDown);
       if ( ! self._initiated ) { return; }
@@ -103,19 +114,84 @@ export var Navigator = Control.extend({
         self._control.value = Math.ceil(self._reader.locations.percentageFromCfi(cfi) * 100);
         self._update();
       }
+=======
+      self._update(location);
+>>>>>>> 8340f58... some bits handling significant navigation
     })
+
+    // this._reader.on('xxrelocated', function(location) {
+    //   console.log("AHOY NAVIGATOR relocated", location, self._initiated, self._mouseDown);
+    //   if ( ! self._initiated ) { return; }
+    //   if ( ! self._mouseDown ) {
+    //     location = self._reader.currentLocation();
+    //     self._control.value = Math.ceil(self._reader.locations.percentageFromCfi(self._reader.currentLocation().start.cfi) * 100);
+    //     self._update();
+    //   }
+    // })
 
   },
 
   _action: function() {
-    var value = this._control.value;
+    var value = parseInt(this._control.value, 10);
+    var cfi;
     var locations = this._reader.locations;
-    var cfi = locations.cfiFromPercentage(value / 100);
+    if ( locations.cfiFromLocation ) {
+      cfi = locations.cfiFromLocation(value);
+    } else {
+      // hopefully short-term compatibility
+      var percent = value / this._total;
+      cfi = locations.cfiFromPercentage(percent);
+    }
     this._reader.tracking.action("navigator/go");
-    this._reader.display(cfi);
+    this._ignore = true;
+    this._reader.gotoPage(cfi);
+  },
+
+  _actionXX: function() {
+    var value = parseInt(this._control.value, 10);
+    var fragment = this._reader.nextFragment(value, value >= this._last_value);
+
+    this._reader.tracking.action("navigator/go");
+    this._control.value = fragment.location;
+    console.log("AHOY NAVIGATOR action", value, this._last_value, fragment.location, fragment.cfi);
+    this._last_value = this._control.value;
+    this._reader.gotoPage(fragment.cfi);
   },
 
   _update: function(current) {
+    var self = this;
+
+    if ( ! this._initiated ) { return ; }
+
+    var rangeBg = this._background;
+    var range = self._control;
+
+    var value = parseFloat(range.value, 10);
+    var current_location = value;
+
+    if ( current_location == this._last_reported_location ) {
+      return;
+    }
+    this._last_reported_location = current_location;
+
+    var max = parseFloat(range.max, 10);
+    var percentage = (( value / max ) * 100.0)
+
+    rangeBg.setAttribute('style', 'background-position: ' + (-percentage) + '% 0%, left top;');
+
+    percentage = Math.ceil(percentage);
+    self._control.setAttribute('data-background-position', percentage);
+    this._spanCurrentPercentage.innerHTML = percentage + '%';
+    this._spanCurrentLocation.innerHTML = ( current_location );
+
+    range.setAttribute('aria-valuenow', value);
+    range.setAttribute('aria-valuetext', `${value}% • Location ${current_location} of ${this._total}`);
+
+    var message = `Location ${current_location}; ${percentage}%`;
+    this._reader.updateLiveStatus(message);
+  },
+
+  _updateXXX: function(current) {
     var self = this;
 
     if ( ! current ) { current = this._reader.currentLocation(); }
@@ -126,10 +202,10 @@ export var Navigator = Control.extend({
       return;
     }
 
-    console.log("AHOY NAVIGATOR update", current.start.cfi);
+    var current_location = self._parseLocation(current);
+    console.log("AHOY NAVIGATOR update", current.start, current_location);
 
     // check this early to avoid emitting events
-    var current_location = this._reader.locations.locationFromCfi(current.start.cfi);
     if ( current_location == this._last_reported_location ) {
       return;
     }
@@ -138,11 +214,14 @@ export var Navigator = Control.extend({
     var rangeBg = this._background;
     var range = self._control;
 
-    var value = parseFloat(range.value, 10);
-    var percentage = value;
+    var value = parseFloat(current_location, 10);
+    var max = parseFloat(range.max, 10);
+    var percentage = (( value / max ) * 100.0)
+    console.log("AHOY NAVIGATOR percentage", value, max, percentage);
 
     rangeBg.setAttribute('style', 'background-position: ' + (-percentage) + '% 0%, left top;');
     self._control.setAttribute('data-background-position', Math.ceil(percentage));
+    percentage = Math.ceil(percentage);
 
     this._spanCurrentPercentage.innerHTML = percentage + '%';
     this._spanCurrentLocation.innerHTML = ( current_location );
@@ -177,7 +256,6 @@ export var Navigator = Control.extend({
   },
 
   _initializeNavigator: function(locations) {
-    console.log("AHOY updateLocations PROCESSING LOCATION");
     this._initiated = true;
 
     if ( ! this._reader.pageList ) {
@@ -185,19 +263,63 @@ export var Navigator = Control.extend({
     }
 
     this._total = this._reader.locations.total;
-    if ( this._reader.currentLocation() && this._reader.currentLocation().start ) {
-      this._control.value = Math.ceil(this._reader.locations.percentageFromCfi(this._reader.currentLocation().start.cfi) * 100);
-      this._last_value = this._control.value;
-    } else {
-      this._last_value = this._control.value;
-    }
+    var max = this._total; var min = 1;
+    if ( this._reader.locations.spine ) { max -= 1; min -= 1; }
+    this._control.max = max; // setAttribute('max', max);
+    this._control.min = min; // setAttribute('min', min);
+
+    var value = this._parseLocation(this._reader.currentLocation());
+    this._control.value = value;
+    this._last_value = this._control.value
+    this._update();
+
+    // var current = this._reader.currentLocation();
+    // if ( current && current.start ) {
+    //   console.log("AHOY updateLocations PROCESSING LOCATION", current);
+    //   // this._control.value = Math.ceil(this._reader.locations.percentageFromCfi(this._reader.currentLocation().start.cfi) * 100);
+    //   if ( typeof(current.start) == 'object' ) {
+    //     if ( current.start.location != null ) {
+    //       this._control.value = current.start.location;
+    //     } else {
+    //       var percentage = this._reader.locations.percentageFromCfi(this._reader.currentLocation().start.cfi);
+    //       this._control.value = Math.ceil(this._total * percentage);
+    //       console.log("AHOY NAVIGATOR initialized", Math.ceil(this._total * percentage), this._control.value);
+    //     }
+    //   } else {
+    //     this._control.value = current.start;
+    //   }
+    //   this._last_value = this._control.value;
+    //   this._update(current);
+
+    // } else {
+    //   this._last_value = this._control.value;
+    //   this._update();
+    // }
 
     this._spanTotalLocations.innerHTML = this._total;
 
-    this._update();
     setTimeout(function() {
       DomUtil.addClass(this._container, 'initialized');
     }.bind(this), 0);
+  },
+
+  _parseLocation: function(location) {
+    var self = this;
+    var value;
+
+    if ( typeof(location.start) == 'object' ) {
+      if ( location.start.location != null ) {
+        value = location.start.location;
+      } else {
+        var percentage = self._reader.locations.percentageFromCfi(location.start.cfi);
+        value = Math.ceil(self._total * percentage);
+      }
+    } else {
+      // PDF bug
+      value = parseInt(location.start, 10);
+    }
+
+    return value;
   },
 
   EOT: true
